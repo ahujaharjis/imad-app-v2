b1b340f4e04964f5bbdb0d3cd709d0e4ad1c97e3
ahujaharjis <ahujaharjis@users.noreply.github.com>
2017-02-08 17:56:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log('Loaded!');
 // to changee elements
 var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
+
 // tpo shift image
 var img = document.getElementById('madi');
 img.onclick=function(){
