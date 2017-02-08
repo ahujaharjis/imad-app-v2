6b9cb5b19176a3ad1f7cf6b5e0b5a9507524620d
ahujaharjis <ahujaharjis@users.noreply.github.com>
2017-02-08 18:06:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
 
 // tpo shift image
+ var marginleft = 0;
+function moveright(){
+    marginleft = marginleft+1;
+    img.style.marginleft = marginlef+'px';
+}
 var img = document.getElementById('madi');
-img.onclick=function(){
-img.style.marginleft = '100px';
-    };
+var interval = setInterval(moveright,100);
     
     
