073430001d5dfe0e598b5a1dabf01b11d956e2bc
ahujaharjis <ahujaharjis@users.noreply.github.com>
2017-02-08 19:11:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,9 @@ var button = document.getElementById('counter');
 var counter = 0;
 
 // to changee elements
+button.onclick = function()
+{
 var span = document.getElementById('count');
 counter = counter+1;
 span.innerHTML = counter.toString();
-
+};
